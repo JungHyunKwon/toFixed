6777a59fb128f477f960c2eb94cc8b991bf3b7f5
JungHyunKwon <kjh_3859@naver.com>
2019-07-21 02:15:44 +0900

    published

--- js/toFixed.js
+++ js/toFixed.js
@@ -2,50 +2,46 @@
  * @author JungHyunKwon
  * @version 1.0.0
  */
-try {
-	(function() {
-		'use strict';
-		
-		/**
-		 * @name 숫자 확인
-		 * @since 2017-12-06
-		 * @param {*} value
-		 * @return {boolean}
-		 */
-		function _isNumeric(value) {
-			return typeof value === 'number' && !isNaN(value) && isFinite(value);
-		}
+(function() {
+	'use strict';
+	
+	/**
+	 * @name 숫자 확인
+	 * @since 2017-12-06
+	 * @param {*} value
+	 * @return {boolean}
+	 */
+	function _isNumeric(value) {
+		return typeof value === 'number' && !isNaN(value) && isFinite(value);
+	}
 
-		/**
-		 * @name toFixed
-		 * @param {number} value
-		 * @param {number} decimal
-		 * @return {number}
-		 * @since 2018-07-13
-		 */
-		window.toFixed = function(value, decimal) {
-			var result = NaN;
+	/**
+	 * @name toFixed
+	 * @param {number} value
+	 * @param {number} decimal
+	 * @return {number}
+	 * @since 2018-07-13
+	 */
+	window.toFixed = function(value, decimal) {
+		var result = NaN;
+		
+		//값이 숫자일 때
+		if(_isNumeric(value)) {
+			result = value;
 			
-			//값이 숫자일 때
-			if(_isNumeric(value)) {
-				result = value;
+			//소수가 숫자일 때
+			if(_isNumeric(decimal)) {
+				var splitValue = value.toString().split('.'),
+					firstSplitValue = splitValue[1];
 				
-				//소수가 숫자일 때
-				if(_isNumeric(decimal)) {
-					var splitValue = value.toString().split('.'),
-						firstOfSplitValue = splitValue[1];
-					
-					//소수점이 있을 때
-					if(firstOfSplitValue) {
-						splitValue[1] = firstOfSplitValue.substring(0, decimal);
-						result = parseFloat(splitValue.join('.'), 10);
-					}
+				//소수점이 있을 때
+				if(firstSplitValue) {
+					splitValue[1] = firstSplitValue.substring(0, decimal);
+					result = parseFloat(splitValue.join('.'), 10);
 				}
 			}
+		}
 
-			return result;
-		};
-	})();
-}catch(e) {
-	console.error(e);
-}
+		return result;
+	};
+})();
